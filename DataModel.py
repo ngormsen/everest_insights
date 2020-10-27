24c95568a8802a58a222ea0acc043df369e86669
Nils <28842354+ngandlau@users.noreply.github.com>
2020-10-27 19:48:27 +0100

    Refactor DataModel

--- DataModel.py
+++ DataModel.py
@@ -1,41 +1,28 @@
 from utils import get_month, get_date
 import pandas as pd
+import plotly.express as px
 
 class DataModel:
     trans_log_raw = None
     trans_log_clean = None
     cohort_data = None
 
-    def __init__(self, transaction_log):
+    def __init__(self, transaction_log, cust_id, timestamp, amount_spent):
+        # Preprocessing
+        df = transaction_log
+        df = self.preprocess_colnames(df, cust_id, timestamp, amount_spent)
+        df = self.preprocess_data_types(df)
+        df = self.add_acquisition_timestamp(df)
+
         self.trans_log_raw = transaction_log
+        self.trans_log_clean = df
 
-    def preprocess(self, cust_id, timestamp, amount_spent):
-        self.rename_columns(cust_id, timestamp, amount_spent)
-        self.convert_data_types()
-
-    def get_cohort_data(self, dep_var="amount_spent", by_period="month"):
-        trans_log = self.trans_log_clean
-        trans_log = self.add_cohort_columns(trans_log=trans_log, period=by_period)
-        cohort_data = self.build_cohort_data(trans_log, dep_var=dep_var)
-        self.cohort_data = cohort_data
-        return cohort_data
-
-    def get_cohort_table(self):
-        cohort_table = self.cohort_data.pivot_table(
-            index = "cohort_id",
-            columns = "order_month",
-            values= "amount_spent"
-        )
-        return cohort_table
-        
-    def rename_columns(self, cust_id, timestamp, amount_spent):
+    def preprocess_colnames(self, trans_log, cust_id, timestamp, amount_spent):
         """
         Select and rename the columns that are relevant for building
         cohorts from a transaction log. This should be done manually by the user.
         """
-        df = self.trans_log_raw
-
-        df.rename(
+        trans_log.rename(
             columns={
                 cust_id: "cust_id",
                 timestamp: "timestamp",
@@ -45,86 +32,96 @@ class DataModel:
         )
 
         # Drop all other Variables
-        df = df[["cust_id", "timestamp", "amount_spent"]]
-        self.trans_log_clean = df
+        trans_log = trans_log[["cust_id", "timestamp", "amount_spent"]]
+        return trans_log
 
-    def convert_data_types(self):
+    def preprocess_data_types(self, trans_log):
         """
-        Converts the columns of the transaction log into correct data types.
+        Converts columns of the transaction log into correct data types.
         """
-        df = self.trans_log_clean
-        df.loc[:, "timestamp"] = pd.to_datetime(df["timestamp"])
-        self.trans_log_clean = df
+        trans_log.loc[:, "timestamp"] = pd.to_datetime(trans_log["timestamp"])
+        return trans_log
 
-    def add_cohort_columns(self, trans_log, period="month"):
+    def add_acquisition_timestamp(self, trans_log):
         """
-        Builds cohorts based on the period chosen.
+        Adds new column for the acquisition date. The acquisition date
+        is the date of the first purchase for each customer.
+        """
+        acq_timestamp = (trans_log
+            .groupby("cust_id")["timestamp"]
+            .min()
+            .reset_index()
+            .rename(columns={"timestamp" : "acq_period"}))
+
+        trans_log = (trans_log
+            .set_index("cust_id")
+            .join(acq_timestamp.set_index("cust_id")))
+
+        acq_years = trans_log["acq_timestamp"].dt.year.apply(str)
+        acq_months = trans_log["acq_timestamp"].dt.month.apply(str)
+        acq_period = acq_years.str.cat(acq_months, sep="-")
+        trans_log["acq_period"] = acq_period
+        return trans_log
+
+    def compute_cohort_ages(self, trans_log, acq_timestamp, order_timestamp, by):
+        """
+        Computes the ages for each cohort given a (cohort) transaction log.
+        
+        Args
+            acq_timestamp: String: Name of the column containing acquisition period
+            order_timestamp: String: Name of the column containing order period
+            by: String: Ages in terms of "month" or "year"
+        """
+        if by == "month":
+            order_years, order_months, _ = get_date(trans_log, order_timestamp)
+            acq_years, acq_months, _ = get_date(trans_log, acq_timestamp)
+            trans_log["age"] = (order_years - acq_years) * 12 + (order_months - acq_months)
+        self.trans_log_clean = trans_log
+        return trans_log
+
+    def translog_by_cohort(self, trans_log, dep_var):
+        """
+        Aggregates a customer-level transaction log into cohort-based data.
 
         Args
-            period: String: One of "month", "year"
-
-        Note: Currently works only for month
+            dep_var: String: One of "count" or "amount_spent"
         """
-        df = trans_log
-        df["order_month"] = df["timestamp"].apply(get_month)
-        df["cohort_month"] = df.groupby("cust_id")["order_month"].transform("min")
-
-        order_year, order_month, _ = get_date(df, 'order_month')
-        cohort_year, cohort_month, _ = get_date(df, 'cohort_month')
-
-        year_diff = order_year - cohort_year
-        month_diff = order_month - cohort_month
-
-        df['cohort_id'] = year_diff * 12 + month_diff + 1
-        return df
-
-    def build_cohort_data(self, trans_log, dep_var):
-        """
-        Builds a Cohort Table from a (cleaned) Transaction Log that already
-        contains columns created by `add_cohort_columns()`.
-
-        Args:
-            dep_var: String: One of "count", "amount_spent"
-        """
-        df = trans_log
-
-        if dep_var == "count":
-            # Counts the number of customers per cohort & month
-            cohort_data = df.groupby(["cohort_id", "order_month"])["cust_id"].agg("count").reset_index()
-
         if dep_var == "amount_spent":
-            cohort_data = df.groupby(["cohort_id", "order_month"])["amount_spent"].sum().reset_index()
+            cohort_trans_log = (trans_log
+                .groupby(["acq_period", "timestamp"])["amount_spent"]
+                .sum()
+                .reset_index()
+                .rename(columns={"acq_period": "cohort"}))
+        return cohort_trans_log
 
-        return cohort_data
-
-    def get_cohort_table(self):
-        cohort_table = self.cohort_data.pivot_table(
-            index = "cohort_id",
-            columns = "order_month",
-            values= "amount_spent"
+    def get_cohort_table(self, cohort_trans_log, dep_var="amount_spent"):
+        cohort_trans_log.pivot_table(
+            index="cohort",
+            columns="timestamp",
+            values=dep_var 
         )
-        return cohort_table
 
-    def get_trans_log(self):
-        return self.trans_log_clean
-
-# ## EXAMPLE
-
-# trans_log = pd.read_csv("data/retail_relay.csv")
-# data = DataModel(transaction_log=trans_log)
-
-# # Data Cleaning
-# data.preprocess(
-#     cust_id="userId",
-#     timestamp="orderDate",
-#     amount_spent="totalCharges"
-# )
-
-# cohort_data = data.get_cohort_data(dep_var="amount_spent", by_period="month")
-# cohort_table = data.get_cohort_table()
-    
-# print(cohort_data)
-# print(cohort_table)
+    def plot_linechart(self, cohort_trans_log, dep_var, view="cohort-age"):
+        """
+        Args
+            view: String: One of "cohort-age" or "cohort-period"
+        """
+        if view == "cohort-age":
+            px.line(
+                cohort_trans_log,
+                x="age",
+                y=dep_var,
+                color="cohort"
+            )
+        elif view == "cohort-period":
+            px.line(
+                cohort_trans_log,
+                x="timestamp",
+                y=dep_var,
+                color="cohort"
+            )
+        else:
+            raise ValueError("Unknown view. Choose cohort-age or cohort-period")
 
 
 
